d148f9038e6a7801c86201b8b041476bb4429239
Petteri <petteri.tuononen@gmail.com>
2026-09-24 11:05:12 +0200

    add original input title to response

--- MyAnimeList.Backend/Models/Response/BulkImportCandidateResponse.cs
+++ MyAnimeList.Backend/Models/Response/BulkImportCandidateResponse.cs
@@ -3,6 +3,7 @@
     public class BulkImportCandidateResponse
     {
         public Anime? Anime { get; set; }
+        public string InputTitle { get; set; } = string.Empty;
         public string Status { get; set; } = string.Empty;
         public int? Score { get; set; }
         public string? Notes { get; set; }
